0754089f9fce0d4dae14e93f2849ede43c1987ff
M. Olcay TERCANLI <molcay@mail.com>
2015-08-10 12:55:20 +0300

    init

--- public_html/js/cards.js
+++
@@ -1,9 +0,0 @@
-function tile(name, number, point, canMove, huntingDirection, prey, image){
-    this.name = name;
-    this.number = number;
-    this.point = point;
-    this.canMove = canMove;
-    this.huntingDirection = huntingDirection;
-    this.prey = prey;
-    this.image = image;
-}
