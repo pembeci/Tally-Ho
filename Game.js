a6b1492b38c87966ab7bfd569ef2d95864c7034d
mersanuzun <mersanuzun@gmail.com>
2015-08-11 11:33:17 +0300

    tile selected class

--- Game.js
+++ Game.js
@@ -7,21 +7,39 @@ board = [
         [ [], [], [], [], [], [], [] ],
         [ [], [], [], [], [], [], [] ]
     ];
-
+var a = new Player("ersan");
+var b = new Player("meltem");
+ 
+var t = new TallyHo();
+t.setBlue(a);
+t.setBrown(b);
+t.createBlues();
+t.createGreens();
+t.createBrowns();
+      
 var ractive = new Ractive({
     el : "#container",
     template : "#template",
     data : {
         board : board,
-        tiles : shuffle(t.tiles)
+        tiles : shuffle(t.tiles),
+        selectedTile : null,
+        selectedTileCoor : null
     }
 })
 ractive.on("showTile", function(e){
     ractive.set(e.keypath + ".visible", true);
 })
-
-ractive.on("move", function(){
-    alert()
+ractive.on("move", function(e){
+    if (this.get('selectedTile')){
+        ractive.set(e.keypath, this.get('selectedTile'));
+        ractive.set(this.get("selectedTileCoor"), null);
+    }
+})
+ractive.on("selectTile", function(e){
+    this.set("selectedTile", this.get(e.keypath));
+    this.set("selectedTileCoor", e.keypath);
+    console.log(this.get("selectedTile"))
 })
 ractive.on("tilePlacement", function(){
     for(var i = 0; i < this.get("board").length; i++){
